c4fcbc468da7fc51a701e4ed8d6e93572e60edc0
Kuldeep Singh <kuldeepsingh21070@gmail.com>
2025-10-30 16:17:15 +0530

    day3 code update

--- Day3-code/script.js
+++ Day3-code/script.js
@@ -1,27 +1,41 @@
-var products = [
-    new Product("Iphone 17", "$399", "Smartphones"),
-    new Product("Mars", "$39", "Chocolate"),
-    new Product("Sapiens", "$39", "Books"),
-    new Product("The Great Gatsby", "$59", "Books")
-]
-
-// console.log(Products)
-
-// products.forEach((product,index) => console.log(product))
-// products.forEach((p) => console.log(p))
-// console.log(products[0])
-
-// console.log(typeof(products))
-
 function Product(name, price, category) {
-    this.name = name
-    this.price = price
-    this.category = category
+    this.name = name;
+    this.price = price;
+    this.category = category;
 }
-
 
 Product.prototype.getDiscountedPrice = function(percent) {
-    var newPrice = this.price - (this.price * percent / 100)
-    return newPrice
-}
+    var newPrice = this.price - (this.price * percent / 100);
+    return newPrice;
+};
 
+(function() {
+    var products = [
+        new Product("Iphone 17", 399, "Smartphones"),
+        new Product("Mars", 39, "Chocolate"),
+        new Product("Sapiens", 49, "Books"),
+        new Product("The Great Gatsby", 59, "Books")
+    ];
+
+    var container = document.getElementById("product-container");
+
+    for (var i = 0; i < products.length; i++) {
+        var card = document.createElement('div');
+        card.className = 'product-card';
+
+        var name = document.createElement('h3');
+        name.textContent = "Product Name: " + products[i].name;
+
+        var price = document.createElement('h3');
+        price.textContent = "Product Price: " + products[i].price;
+
+        var category = document.createElement('h3');
+        category.textContent = "Product Category: " + products[i].category;
+
+        card.appendChild(name);
+        card.appendChild(price);
+        card.appendChild(category);
+
+        container.appendChild(card);
+    }
+})();
