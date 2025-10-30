b86fdd1f9c52aa08bec959f11ac0f2e01d02337c
Kuldeep Singh <kuldeepsingh21070@gmail.com>
2025-10-30 16:29:13 +0530

    day3

--- Day3-code/script.js
+++ Day3-code/script.js
@@ -7,7 +7,22 @@ function Product(name, price, category) {
 Product.prototype.getDiscountedPrice = function(percent) {
     var newPrice = this.price - (this.price * percent / 100);
     return newPrice;
-};
+}
+
+
+function getCostliestProduct(products) {
+    var max = 0
+    var costliest = null
+
+    for(var i=0;i<products.length;++i){
+        if(products[i].price > max){
+            max = products[i].price
+            costliest = products[i]
+        }
+    }
+
+    return costliest
+}
 
 (function() {
     var products = [
@@ -20,22 +35,39 @@ Product.prototype.getDiscountedPrice = function(percent) {
     var container = document.getElementById("product-container");
 
     for (var i = 0; i < products.length; i++) {
-        var card = document.createElement('div');
+        var card = document.createElement('div')
         card.className = 'product-card';
 
-        var name = document.createElement('h3');
-        name.textContent = "Product Name: " + products[i].name;
+        var name = document.createElement('h3')
+        name.textContent = "Product Name: " + products[i].name
 
-        var price = document.createElement('h3');
-        price.textContent = "Product Price: " + products[i].price;
+        var price = document.createElement('h3')
+        price.textContent = "Product Price: " + products[i].price
 
-        var category = document.createElement('h3');
-        category.textContent = "Product Category: " + products[i].category;
+        var category = document.createElement('h3')
+        category.textContent = "Product Category: " + products[i].category
 
         card.appendChild(name);
         card.appendChild(price);
         card.appendChild(category);
 
+        card.addEventListener('mouseenter', function() {
+            this.style.backgroundColor = "lightGreen"
+        })
+
+        card.addEventListener('mouseleave', function() {
+            this.style.backgroundColor = 'azure'
+        })
+
         container.appendChild(card);
     }
+
+
+    var costliestProduct = getCostliestProduct.apply(null, [products]);
+
+    document.getElementById("costliest").textContent = 
+    `Costliest Product is ${costliestProduct.name} 
+     Priced at $${costliestProduct.price}
+    `
+
 })();
